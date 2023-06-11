f33cde2cc87cba17740e83ad933a89d204dbec12
Mkkpy <135785456+Mkkpy@users.noreply.github.com>
2023-06-11 17:11:08 -0300

    Update ex011.py

--- ex011.py
+++ ex011.py
@@ -1,3 +1,3 @@
-salario = float(input('Qual é o salario do funcionarop? R$ '))
+salario = float(input('Qual é o salario do funcionaro? R$ '))
 aumento = salario + (salario * 15 / 100)
 print('Um funcionario que ganhava R$ {:.2f}, com 15% de aumento, passa a receber R% {:.2f}'.format(salario, aumento))
